b22d1794247a4293bc52364553221c5e0f2f28d5
safiya4510 <safiyasara6363@gmail.com>
2026-09-20 19:13:17 +0530

    Add Registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import OceanLayout from './components/layout/OceanLayout/OceanLayout';
 import LandingPage from './features/auth/pages/LandingPage';
+import Registration from './features/auth/registration/Registration';
 import Dashboard from './features/primary/dashboard/Dashboard';
 import Courses from './features/primary/courses/Courses';
 import Modules from './features/primary/modules/Modules';
@@ -28,6 +29,7 @@ function App() {
           <Router>
         <Routes>
           <Route path="/landing" element={<LandingPage />} />
+          <Route path="/registration" element={<Registration />} />
           
           {/* Protected routes wrapped in OceanLayout */}
           <Route path="/" element={<OceanLayout />}>
